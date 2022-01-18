bdce76562cf8d271613a44f3add7d351ebe63c3c
rbmoon <rbmoon68@yahoo.com>
2022-01-18 19:16:08 +0330

    a

--- src/main/java/org/ce/ap/client/Main.java
+++ src/main/java/org/ce/ap/client/Main.java
@@ -2,6 +2,7 @@ package main.java.org.ce.ap.client;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Worffld");
+
     }
 }
